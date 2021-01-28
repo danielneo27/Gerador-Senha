1b594ea3dd9d8cb4d1562ef64dd12c62a7a85c05
Daniel Neo <78002139+danielneo27@users.noreply.github.com>
2021-01-28 12:17:50 -0300

    Add files via upload

---
+++ Gerador de senha [Complexa].ps1
@@ -0,0 +1,37 @@
+﻿<#	
+	.NOTAS
+	===========================================================================
+	 Criado em:   	07/01/2021 15:33
+	 Criado por:   	Daniel Neo
+     Versão:        1.0.0
+     Atualizado:    19/01/2021
+	 Nome do arquivo:  Gerador de senha[Complexa]
+	===========================================================================
+	.DESCRIÇÃO
+		Crie senhas seguras e personalizadas com esta ferramenta de geração Complexa.
+
+        Altere neste campo o numero de caracteres especiais desejados "($PasswordLenght,3)"
+        
+        Para gerar uma senha com mais de 10 caracteres altere o valor de  "GenerateStrongPassword (10)"
+#>
+Function GenerateStrongPassword ([Parameter(Mandatory=$true)][int]$PasswordLenght)
+{
+Add-Type -AssemblyName System.Web
+$PassComplexCheck = $false
+do {
+$newPassword=[System.Web.Security.Membership]::GeneratePassword($PasswordLenght,3)
+If ( ($newPassword -cmatch "[A-Z\p{Lu}\s]") `
+-and ($newPassword -cmatch "[a-z\p{Ll}\s]") `
+-and ($newPassword -match "[\d]") `
+-and ($newPassword -match "[^\w]")
+)
+{
+$PassComplexCheck=$True
+}
+} While ($PassComplexCheck -eq $false)
+return $newPassword
+}
+
+GenerateStrongPassword (10)
+
+pause
---
+++ Gerador de senha [Simples].ps1
@@ -0,0 +1,17 @@
+﻿<#	
+	.NOTAS
+	===========================================================================
+	 Criado em:   	06/01/2021 18:10
+	 Criado por:   	Daniel Neo
+	 Nome do arquivo:  Gerador de senha[Simples]
+	===========================================================================
+	.DESCRIÇÃO
+		Crie senhas seguras e personalizadas com esta ferramenta de geração simples.
+#>
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+Add-Type -AssemblyName System.Web
+Write-Host "Senha gerada com Sucesso!"
+#Escolha o padrão abaixo no campo entre parenteses.
+[System.Web.Security.Membership]::GeneratePassword(10, 3)
+
+Pause
